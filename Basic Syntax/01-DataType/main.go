c92e7a5bda9fa5364880394efa6940024f9b745d
ZhangYangDev <1151559314@qq.com>
2019-09-11 00:41:18 +0800

    no message

--- Basic Syntax/01-DataType/main.go
+++ Basic Syntax/01-DataType/main.go
@@ -1,45 +1,46 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"reflect"
+)
 
 // 变量 & 数据类型 & 常量
 
 func main() {
 	// 数值类型
-	fmt.Println("--------------- 基本数据类型 Start --------------")
+	fmt.Println("--------------- 数值类型 --------------")
 
+	// int 
+	var num1 int8 = -82
+	var num2 uint16= 32834
+
+	fmt.Printf("num1 type:", reflect.TypeOf(num1))
+	fmt.Printf("\nnum2 type:", reflect.TypeOf(num2))
+	// float
+	var num3 float64 = 231.4356
+	var num4 complex128 = 23434.4324
+	fmt.Printf("\nnum3 type:", reflect.TypeOf(num3))	
+	fmt.Printf("\nnum4 type:", reflect.TypeOf(num4))	
+
+	// rune  类似于int32
+	var num5 rune = 554
+	fmt.Println("\nnum5 type:", reflect.TypeOf(num5))
+	
 	// 布尔型
 	var flag bool = true
-	fmt.Println("bool|char=>", flag)
+	fmt.Println("\n--------------- 布尔型 --------------")
+	fmt.Println("\nflag type:", reflect.TypeOf(flag))
+	
 	// 字符串
 	// string  官方将String归类为基本数据类型
+	var str = "GoLang"
+	fmt.Println("\n--------------- 字符串 --------------")
+	fmt.Printf("\nstr type:", reflect.TypeOf(str))
 
-	var str string = "GoLang"
-	fmt.Println("string:str=>", str)
-	fmt.Println("--------------- 基本数据类型 End --------------")
+	// 字符型 
+	fmt.Println("\n--------------- 字符型 --------------")
+	var bit byte = 'm'
+	fmt.Printf("\nbit type:", reflect.TypeOf(bit))
 
-}
-
-func type_number() {
-	fmt.Println("数值类型")
-
-	// 数值型  int8 int16 int32 int64  uint8 uint16 uint32 uint64
-	// int 有符号  uint 无符号
-	// 参考文档: https://studygolang.com/articles/7902
-	var num1 int8 = -123
-	fmt.Println("int8|num1=>", num1)
-	var num2 uint8 = 123
-	fmt.Println("unit8|num2=>", num2)
-	// float 浮点型 float32 float64
-	// GoLang的浮点型默认声明为64位  float64的精度要比float32的精度准确
-	var fl float64 = 1.4967
-	fmt.Println("float64|fl=>", fl)
-}
-
-func type_byte() {
-	fmt.Println("字符型Byte===>")
-	// 字符型
-	// byte 可以存单个字母 类似于Java中的char
-	var char byte = 'a'
-	fmt.Println("byte|char=>", char)
 }
--- Basic Syntax/02-Operator/main.go
+++ Basic Syntax/02-Operator/main.go
@@ -4,7 +4,12 @@ import(
 	"fmt"
 )
 
+const(
+	num1 uint8 = 100
+	num2 uint8 = 88
+)
+
 func main(){
-	
 	// 运算符
+	fmt.Println(num1+num2)
 }
