4b6e483937e6c7818a6a9627c4c67beb8f1d2f13
ZhangYangDev <1151559314@qq.com>
2019-10-07 23:37:01 +0800

    no message

--- Basic Syntax/10-OOP/main.go
+++ Basic Syntax/10-OOP/main.go
@@ -4,7 +4,30 @@ import (
 	"fmt"
 )
 
+// 创建结构体
+type User struct{
+	name string
+	age uint8
+	gender string 
+	desc string 
+}
+
+
+func (user *User) say() string{
+
+	uInfo := fmt.Sprintf(user.name,user.age,user.gender,user.desc)
+
+	return uInfo
+}
+
+
 
 func main(){
-	fmt.Println("OOP)
+	var us = User{
+		name:"Obj",
+		age:30,
+		gender:"male",
+		desc:"sth",
+	}
+	fmt.Println(us.say())
 }
---
+++ Basic Syntax/11-Reflect/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Reflect => 反射")
+}
